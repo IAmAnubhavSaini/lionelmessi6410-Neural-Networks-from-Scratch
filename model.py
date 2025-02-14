6de1d795d8f2aafa91ce10541a75f70ee0e30fac
nopeasti <hello+git@anubhavsaini.com>
2025-02-14 11:34:44 +0530

    read changes below:
    1. introduced .gitignore
    2. introduced .editorconfig
    3. formatted train.py
    4. formatted model.py

--- model.py
+++ model.py
@@ -1,27 +1,29 @@
 import numpy as np
 import time
 
+
 class DeepNeuralNetwork():
     def __init__(self, sizes, activation='sigmoid'):
         self.sizes = sizes
-        
+
         # Choose activation function
         if activation == 'relu':
             self.activation = self.relu
         elif activation == 'sigmoid':
             self.activation = self.sigmoid
         else:
-            raise ValueError("Activation function is currently not support, please use 'relu' or 'sigmoid' instead.")
-        
+            raise ValueError(
+                "Activation function is currently not support, please use 'relu' or 'sigmoid' instead.")
+
         # Save all weights
         self.params = self.initialize()
         # Save all intermediate values, i.e. activations
         self.cache = {}
-        
+
     def relu(self, x, derivative=False):
         '''
             Derivative of ReLU is a bit more complicated since it is not differentiable at x = 0
-        
+
             Forward path:
             relu(x) = max(0, x)
             In other word,
@@ -42,7 +44,7 @@ class DeepNeuralNetwork():
         '''
             Forward path:
             σ(x) = 1 / 1+exp(-z)
-            
+
             Backward path:
             ∇σ(x) = exp(-z) / (1+exp(-z))^2
         '''
@@ -60,10 +62,10 @@ class DeepNeuralNetwork():
 
     def initialize(self):
         # number of nodes in each layer
-        input_layer=self.sizes[0]
-        hidden_layer=self.sizes[1]
-        output_layer=self.sizes[2]
-        
+        input_layer = self.sizes[0]
+        hidden_layer = self.sizes[1]
+        output_layer = self.sizes[2]
+
         params = {
             "W1": np.random.randn(hidden_layer, input_layer) * np.sqrt(1./input_layer),
             "b1": np.zeros((hidden_layer, 1)) * np.sqrt(1./input_layer),
@@ -71,7 +73,7 @@ class DeepNeuralNetwork():
             "b2": np.zeros((output_layer, 1)) * np.sqrt(1./hidden_layer)
         }
         return params
-    
+
     def initialize_momemtum_optimizer(self):
         momemtum_opt = {
             "W1": np.zeros(self.params["W1"].shape),
@@ -86,12 +88,14 @@ class DeepNeuralNetwork():
             y = σ(wX + b)
         '''
         self.cache["X"] = x
-        self.cache["Z1"] = np.matmul(self.params["W1"], self.cache["X"].T) + self.params["b1"]
+        self.cache["Z1"] = np.matmul(
+            self.params["W1"], self.cache["X"].T) + self.params["b1"]
         self.cache["A1"] = self.activation(self.cache["Z1"])
-        self.cache["Z2"] = np.matmul(self.params["W2"], self.cache["A1"]) + self.params["b2"]
+        self.cache["Z2"] = np.matmul(
+            self.params["W2"], self.cache["A1"]) + self.params["b2"]
         self.cache["A2"] = self.softmax(self.cache["Z2"])
         return self.cache["A2"]
-    
+
     def back_propagate(self, y, output):
         '''
             This is the backpropagation algorithm, for calculating the updates
@@ -99,13 +103,13 @@ class DeepNeuralNetwork():
 
             Note: There is a stability issue that causes warnings. This is 
                   caused  by the dot and multiply operations on the huge arrays.
-                  
+
                   RuntimeWarning: invalid value encountered in true_divide
                   RuntimeWarning: overflow encountered in exp
                   RuntimeWarning: overflow encountered in square
         '''
         current_batch_size = y.shape[0]
-        
+
         dZ2 = output - y.T
         dW2 = (1./current_batch_size) * np.matmul(dZ2, self.cache["A1"].T)
         db2 = (1./current_batch_size) * np.sum(dZ2, axis=1, keepdims=True)
@@ -117,7 +121,7 @@ class DeepNeuralNetwork():
 
         self.grads = {"W1": dW1, "b1": db1, "W2": dW2, "b2": db2}
         return self.grads
-    
+
     def cross_entropy_loss(self, y, output):
         '''
             L(y, ŷ) = −∑ylog(ŷ).
@@ -126,12 +130,12 @@ class DeepNeuralNetwork():
         m = y.shape[0]
         l = -(1./m) * l_sum
         return l
-                
+
     def optimize(self, l_rate=0.1, beta=.9):
         '''
             Stochatic Gradient Descent (SGD):
             θ^(t+1) <- θ^t - η∇L(y, ŷ)
-            
+
             Momentum:
             v^(t+1) <- βv^t + (1-β)∇L(y, ŷ)^t
             θ^(t+1) <- θ^t - ηv^(t+1)
@@ -141,29 +145,32 @@ class DeepNeuralNetwork():
                 self.params[key] = self.params[key] - l_rate * self.grads[key]
         elif self.optimizer == "momentum":
             for key in self.params:
-                self.momemtum_opt[key] = (beta * self.momemtum_opt[key] + (1. - beta) * self.grads[key])
-                self.params[key] = self.params[key] - l_rate * self.momemtum_opt[key]
+                self.momemtum_opt[key] = (
+                    beta * self.momemtum_opt[key] + (1. - beta) * self.grads[key])
+                self.params[key] = self.params[key] - \
+                    l_rate * self.momemtum_opt[key]
         else:
-            raise ValueError("Optimizer is currently not support, please use 'sgd' or 'momentum' instead.")
+            raise ValueError(
+                "Optimizer is currently not support, please use 'sgd' or 'momentum' instead.")
 
     def accuracy(self, y, output):
         return np.mean(np.argmax(y, axis=-1) == np.argmax(output.T, axis=-1))
 
-    def train(self, x_train, y_train, x_test, y_test, epochs=10, 
+    def train(self, x_train, y_train, x_test, y_test, epochs=10,
               batch_size=64, optimizer='momentum', l_rate=0.1, beta=.9):
         # Hyperparameters
         self.epochs = epochs
         self.batch_size = batch_size
         num_batches = -(-x_train.shape[0] // self.batch_size)
-        
+
         # Initialize optimizer
         self.optimizer = optimizer
         if self.optimizer == 'momentum':
             self.momemtum_opt = self.initialize_momemtum_optimizer()
-        
+
         start_time = time.time()
         template = "Epoch {}: {:.2f}s, train acc={:.2f}, train loss={:.2f}, test acc={:.2f}, test loss={:.2f}"
-        
+
         # Train
         for i in range(self.epochs):
             # Shuffle
@@ -177,7 +184,7 @@ class DeepNeuralNetwork():
                 end = min(begin + self.batch_size, x_train.shape[0]-1)
                 x = x_train_shuffled[begin:end]
                 y = y_train_shuffled[begin:end]
-                
+
                 # Forward
                 output = self.feed_forward(x)
                 # Backprop
@@ -194,4 +201,5 @@ class DeepNeuralNetwork():
             output = self.feed_forward(x_test)
             test_acc = self.accuracy(y_test, output)
             test_loss = self.cross_entropy_loss(y_test, output)
-            print(template.format(i+1, time.time()-start_time, train_acc, train_loss, test_acc, test_loss))
+            print(template.format(i+1, time.time()-start_time,
+                  train_acc, train_loss, test_acc, test_loss))
